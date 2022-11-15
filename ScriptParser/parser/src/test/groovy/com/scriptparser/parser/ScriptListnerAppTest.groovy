99433d28d9c692523b04962e94321cc20defb001
wecracy <wecracy@gmail.com>
2022-11-16 01:15:41 +0900

    Hold Testing Past Test Scenario Examples

--- ScriptParser/parser/src/test/groovy/com/scriptparser/parser/ScriptListnerAppTest.groovy
+++ ScriptParser/parser/src/test/groovy/com/scriptparser/parser/ScriptListnerAppTest.groovy
@@ -35,10 +35,12 @@ class ScriptListnerAppTest extends spock.lang.Specification {
         
         where:
             script            || teamNum | serviceNum | modeNum | transitionNum
-            "cooperation.bdl" || 3       | 16         | 9       | 3
-            "indoor.bdl"      || 1       | 9          | 18      | 4
-            "on_the_move.bdl" || 1       | 9          | 11      | 4
-            "swarm.bdl"       || 1       | 11         | 9       | 5
-            "single.bdl"      || 1       | 7          | 5       | 1
+            "demo_3type.bdl"  || 3       | 10         | 7       | 3
+            "demo_6num.bdl"   || 1       | 4          | 4       | 2
+            //"cooperation.bdl" || 3       | 16         | 9       | 3
+            //"indoor.bdl"      || 1       | 9          | 18      | 4
+            //"on_the_move.bdl" || 1       | 9          | 11      | 4
+            //"swarm.bdl"       || 1       | 11         | 9       | 5
+            //"single.bdl"      || 1       | 7          | 5       | 1
     }
 }
