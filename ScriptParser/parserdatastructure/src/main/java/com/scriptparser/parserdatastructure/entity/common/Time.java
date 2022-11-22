6d092bd9fabebd900e7c33aee699503491c7c60f
wecracy <wecracy@gmail.com>
2022-11-22 11:41:54 +0900

    Time Unit Converter

--- ScriptParser/parserdatastructure/src/main/java/com/scriptparser/parserdatastructure/entity/common/Time.java
+++ ScriptParser/parserdatastructure/src/main/java/com/scriptparser/parserdatastructure/entity/common/Time.java
@@ -9,4 +9,18 @@ import lombok.Getter;
 public class Time {
     private int time;
     private TimeUnit timeUnit;
+
+    public int getConvertedTime() {
+        if (timeUnit == TimeUnit.DAY) {
+            return time * 24;
+        }
+        return time;
+    }
+
+    public TimeUnit getConvertedTimeUnit() {
+        if (timeUnit == TimeUnit.DAY) {
+            return TimeUnit.HOUR;
+        }
+        return timeUnit;
+    }
 }
--- ScriptParser/parserdatastructure/src/main/java/com/scriptparser/parserdatastructure/enumeration/TimeUnit.java
+++ ScriptParser/parserdatastructure/src/main/java/com/scriptparser/parserdatastructure/enumeration/TimeUnit.java
@@ -1,5 +1,15 @@
 package com.scriptparser.parserdatastructure.enumeration;
 
 public enum TimeUnit {
-    MSEC, SEC, MINUTE, HOUR, DAY
+    MSEC("MS"), SEC("S"), MINUTE("M"), HOUR("H"), DAY("D");
+
+    private String value;
+
+    private TimeUnit(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
