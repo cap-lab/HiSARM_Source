9fcbc664401906de9f321d705dd60e0edb4414bb
wecracy <wecracy@gmail.com>
2023-02-20 15:10:01 +0900

    Fix the Test Case

--- ScriptParser/parser/src/test/groovy/com/scriptparser/parser/ScriptListnerAppTest.groovy
+++ ScriptParser/parser/src/test/groovy/com/scriptparser/parser/ScriptListnerAppTest.groovy
@@ -34,9 +34,9 @@ class ScriptListnerAppTest extends spock.lang.Specification {
             mission.getTransitionList().size() == transitionNum
         
         where:
-            script            || teamNum | serviceNum | modeNum | transitionNum
-            "demo_3type.bdl"  || 3       | 10         | 7       | 3
-            "demo_6num.bdl"   || 1       | 5          | 4       | 2
+            script                      || teamNum | serviceNum | modeNum | transitionNum
+            "demo_3type.bdl"            || 3       | 9          | 7       | 3
+            "demo_6num.bdl"             || 1       | 4          | 4       | 2
             "reference/cooperation.bdl" || 3       | 15         | 9       | 3
             "reference/indoor.bdl"      || 1       | 8          | 18      | 4
             "reference/on_the_move.bdl" || 1       | 8          | 11      | 4
