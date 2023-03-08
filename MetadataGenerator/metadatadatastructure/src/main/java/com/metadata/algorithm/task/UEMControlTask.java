9daad12df70dd4994de4aae32c20d8526dd26373
wecracy <wecracy@gmail.com>
2023-03-08 13:25:05 +0900

    Group Port Considered When Action Tasks are Made

--- MetadataGenerator/metadatadatastructure/src/main/java/com/metadata/algorithm/task/UEMControlTask.java
+++ MetadataGenerator/metadatadatastructure/src/main/java/com/metadata/algorithm/task/UEMControlTask.java
@@ -100,14 +100,6 @@ public class UEMControlTask extends UEMTask {
             channelList.add(
                     UEMChannel.makeChannel(this, port, actionTask, (UEMChannelPort) counterPort));
         }
-        if (actionTask.getActionImpl().getActionType().isGroupAction()) {
-            UEMChannelPort port = new UEMChannelPort();
-            port.setPortInfo(actionTask.getName(), actionTask.getGroupPort());
-            getPort().add(port);
-            channelList
-                    .add(UEMChannel.makeChannel(this, port, actionTask, actionTask.getGroupPort()));
-            gorupPortMap.put(actionTask, port);
-        }
         return channelList;
     }
 
