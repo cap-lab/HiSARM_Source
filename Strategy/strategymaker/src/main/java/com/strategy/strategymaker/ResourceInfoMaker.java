3568669ab3f330e9548d8ea2892de9607c5b8dbe
wecracy <wecracy@gmail.com>
2023-09-18 12:07:17 +0900

    Support Virtual Resource

--- Strategy/strategymaker/src/main/java/com/strategy/strategymaker/ResourceInfoMaker.java
+++ Strategy/strategymaker/src/main/java/com/strategy/strategymaker/ResourceInfoMaker.java
@@ -10,6 +10,30 @@ import com.strategy.strategydatastructure.wrapper.ResourceWrapper;
 import com.strategy.strategydatastructure.wrapper.RobotImplWrapper;
 
 public class ResourceInfoMaker {
+
+    private static ResourceWrapper setResourceToResourceWrapper(RobotImplWrapper robot,
+            String resourceId) {
+        ResourceWrapper resource = new ResourceWrapper();
+        resource.setResource(DBService
+                .getResource(robot.getRobotType().getRobotType().getRobotClass(), resourceId));
+        resource.setTask(DBService.getTask(resource.getResource().getTaskId()));
+        return resource;
+    }
+
+    private static void addResourceToRobot(RobotImplWrapper robot, Set<String> resourceSet,
+            String resourceId) {
+        if (!resourceSet.contains(resourceId)) {
+            resourceSet.add(resourceId);
+            ResourceWrapper resource = setResourceToResourceWrapper(robot, resourceId);
+            robot.getResourceList().add(resource);
+            if (!resource.getResource().getRequiredResources().isEmpty()) {
+                for (String requiredResourceId : resource.getResource().getRequiredResources()) {
+                    addResourceToRobot(robot, resourceSet, requiredResourceId);
+                }
+            }
+        }
+    }
+
     public static void makeResourceInfoList(List<RobotImplWrapper> robotList) {
         try {
             for (RobotImplWrapper robot : robotList) {
@@ -17,16 +41,7 @@ public class ResourceInfoMaker {
                 for (ControlStrategyWrapper cs : robot.getControlStrategyList()) {
                     for (ActionImplWrapper action : cs.getActionList()) {
                         for (String resourceId : action.getActionImpl().getResource()) {
-                            if (!resourceSet.contains(resourceId)) {
-                                resourceSet.add(resourceId);
-                                ResourceWrapper resource = new ResourceWrapper();
-                                resource.setResource(DBService.getResource(
-                                        robot.getRobotType().getRobotType().getRobotClass(),
-                                        resourceId));
-                                resource.setTask(
-                                        DBService.getTask(resource.getResource().getTaskId()));
-                                robot.getResourceList().add(resource);
-                            }
+                            addResourceToRobot(robot, resourceSet, resourceId);
                         }
                     }
                 }
