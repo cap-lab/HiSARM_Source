c967bd9198255097289dbfae09266ca7da1b2030
wecracy <wecracy@gmail.com>
2024-04-02 10:57:03 +0900

    Make Default Constructor for Json Parsing

--- Strategy/strategydatastructure/src/main/java/com/strategy/strategydatastructure/additionalinfo/ControlStrategyInfo.java
+++ Strategy/strategydatastructure/src/main/java/com/strategy/strategydatastructure/additionalinfo/ControlStrategyInfo.java
@@ -1,32 +1,42 @@
 package com.strategy.strategydatastructure.additionalinfo;
 
+
 public class ControlStrategyInfo {
     private String actionName;
     private String teamName;
     private String robotClass;
     private String strategyId;
 
-    public ControlStrategyInfo(String actionName, String teamName, String robotClass,
-            String strategyId) {
-        this.actionName = actionName;
-        this.teamName = teamName;
-        this.robotClass = robotClass;
-        this.strategyId = strategyId;
-    }
-
     public String getActionName() {
         return actionName;
+    }
+
+    public void setActionName(String actionName) {
+        this.actionName = actionName;
     }
 
     public String getTeamName() {
         return teamName;
     }
 
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
     public String getRobotClass() {
         return robotClass;
+    }
+
+    public void setRobotClass(String robotClass) {
+        this.robotClass = robotClass;
     }
 
     public String getStrategyId() {
         return strategyId;
     }
+
+    public void setStrategyId(String strategyId) {
+        this.strategyId = strategyId;
+    }
+
 }
